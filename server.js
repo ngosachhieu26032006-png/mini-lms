f090b4e5229d908b6b555615e49d863c030304b7
ten_cua_ban <email_cua_ban>
2026-04-16 08:58:31 +0700

    fix show html

--- server.js
+++ server.js
@@ -5,15 +5,35 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
 
-// CONNECT DB (LOCAL)
+// CONNECT LOCAL DB
 mongoose.connect("mongodb://127.0.0.1:27017/lms")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log("DB Error:", err));
 
-// TEST API
+// MODEL QUIZ
+const Quiz = mongoose.model("Quiz", {
+  question: String,
+  options: [String],
+  correct: Number
+});
+
+// API TEST
 app.get("/", (req, res) => {
-  res.send("API đang chạy OK 🚀");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// ADD QUIZ
+app.post("/quiz", async (req, res) => {
+  const quiz = await Quiz.create(req.body);
+  res.json(quiz);
+});
+
+// GET QUIZ
+app.get("/quiz", async (req, res) => {
+  const data = await Quiz.find();
+  res.json(data);
 });
 
 // RUN SERVER
